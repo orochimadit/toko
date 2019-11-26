aa2455d01e5c278562a116bc24f32ed9e96b9f5e
Aditya Pratama Badra <orochimadit@gmail.com>
2019-11-26 14:50:17 +0700

    isi model barang

--- app/M_Barang.php
+++ app/M_Barang.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class M_Barang extends Model
 {
     //
+    public $timestamps = false;
+    protected $table = "tbl_barang";
+    protected $fillable = ['nama_produk','harga','gambar'];
 }
